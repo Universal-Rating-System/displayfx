5288cd5696f7ebb658c563496c28cdc1761fe171
Hendrik du Toit <hendrik@brightedge.co.za>
2025-02-01 21:27:49 +0200

    Changed Python requirement

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 Write-Host "Running $env:PROJECT_DIR\install.ps1..."  -ForegroundColor Yellow
+git remote set-url origin https://$env:GH_REPO_ACCESS_BY_OWN_APPS@github.com/$env:VENV_ORGANIZATION_NAME/$env:PROJECT_NAME
 (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python -
 poetry init
 if (Test-Path -Path "$env:PROJECT_DIR\pyproject.toml")
